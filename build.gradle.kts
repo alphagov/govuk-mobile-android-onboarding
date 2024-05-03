c8e1a17b10e8103fa37885d5703a8fd3966f2348
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2024-05-03 13:31:50 +0100

    Add basic CI workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,9 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     alias(libs.plugins.androidLibrary) apply false
+    id("jacoco")
+    id("org.sonarqube") version "4.4.1.3373"
 }
+
+apply("sonar.gradle")
+apply("jacoco.gradle")
